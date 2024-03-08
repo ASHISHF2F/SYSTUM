6669a581c0d41528f7f032d1ef5c3f8af6ab45fe
ASHISHF2F <162687803+ASHISHF2F@users.noreply.github.com>
2024-03-08 16:33:22 +0530

    Update play.py

--- SYSTUM/utils/inline/play.py
+++ SYSTUM/utils/inline/play.py
@@ -72,7 +72,7 @@ def stream_markup_timer(_, chat_id, played, dur):
                 text="ᴏᴡɴᴇʀ", url="https://t.me/EROXXXR",
             ),
             InlineKeyboardButton(
-                text="sᴜᴩᴩᴏʀᴛ", url="https://t.me/FRIENDS2FAMILY00",
+                text="ᴄʜᴀᴛ ɢʀᴏᴜᴘ", url="https://t.me/BRANDED_WORLD",
             )
         ],
          [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -94,7 +94,7 @@ def stream_markup(_, chat_id):
                 text="ᴏᴡɴᴇʀ", url="https://t.me/EROXXXR",
             ),
             InlineKeyboardButton(
-                text="sᴜᴩᴩᴏʀᴛ", url="https://t.me/FRIENDS2FAMILY00",
+                text="ᴄʜᴀᴛ ɢʀᴏᴜᴘ", url="https://t.me/BRANDED_WORLD",
             )
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
